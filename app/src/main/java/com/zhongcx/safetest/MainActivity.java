c0357908101f53bcdbea68ec7dcbd4115a1b44df
仲丛旭B <wechat_5d99aff93c735d00010c3b08@mail.teambition.com>
2020-06-13 15:47:32 +0800

    追加“安全键盘”与“二次篡改”测试

--- app/src/main/java/com/zhongcx/safetest/MainActivity.java
+++ app/src/main/java/com/zhongcx/safetest/MainActivity.java
@@ -1,23 +1,33 @@
 package com.zhongcx.safetest;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hqumath.keyboard.KeyboardTool;
+import com.hqumath.keyboard.KeyboardUtil;
+import com.hqumath.keyboard.MyKeyboardView;
+
 import java.io.File;
+import java.io.FileInputStream;
+import java.math.BigInteger;
+import java.security.MessageDigest;
 
 public class MainActivity extends AppCompatActivity {
     private TextView tv_main_screenshot;
-
+    private KeyboardUtil keyboardUtil;
+    private EditText et_main_safe_key;
     //手机允许/不允许截屏按钮
     private View.OnClickListener mScreenshotOnClickListener = new View.OnClickListener() {
         @Override
@@ -38,19 +48,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 01 禁止【电脑模拟器】
+        // 01 禁止【电脑模拟器】测试
         TextView tv_main_env = findViewById(R.id.tv_main_env);
         tv_main_env.setText("运行环境：" + (isEmulator() ? "模拟器" : "真机"));
         TextView tv_main_root = findViewById(R.id.tv_main_root);
         tv_main_root.setText("当前环境是否root：" + (checkRoot() ? "是" : "否"));
 
-        // 02 禁止【手机截屏/录屏】
+        // 02 禁止【手机截屏/录屏】测试
         tv_main_screenshot = findViewById(R.id.tv_main_screenshot);
         tv_main_screenshot.setText("当前截屏/录屏状态：允许");
         Button btn_main_screenshot = findViewById(R.id.btn_main_screenshot);
         btn_main_screenshot.setOnClickListener(mScreenshotOnClickListener);
 
-        // 03 禁止
+        // 03 禁止【release版本下打印输出日志】测试
         Button btn_main_log = findViewById(R.id.btn_main_log);
         btn_main_log.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,11 +68,69 @@ public class MainActivity extends AppCompatActivity {
                 Log.i(MainActivity.this.getClass().getName(), "==测试");
             }
         });
+
+        // 04 禁止【认证信息使用不安全键盘】测试
+        et_main_safe_key = findViewById(R.id.et_main_safe_key);
+        keyboardUtil = new KeyboardUtil(this, findViewById(R.id.ll_main_content));
+        keyboardUtil.initKeyboard(MyKeyboardView.KEYBOARDTYPE_Only_Num_Pwd, false, et_main_safe_key);//安全键盘
+
+        // 05 禁止【apk被篡改后二次打包】测试
+        TextView tv_main_sha = findViewById(R.id.tv_main_sha);
+        tv_main_sha.setText("当前sha值："+ checkApkSha());
     }
 
+    /**
+     * 点击空白处隐藏键盘
+     */
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+            KeyboardTool.isTouchView(null, ev);
+            View v = getCurrentFocus();
+            if (KeyboardTool.isFocusEditText(v)//当前焦点在系统键盘EditText
+                    && !KeyboardTool.isTouchView(new View[]{}, ev)) {//且没有触摸在系统键盘EditText
+                KeyboardTool.hideInputForce(MainActivity.this, v);//隐藏系统键盘
+                v.clearFocus();//清空焦点
+            } else if (KeyboardTool.isFocusEditText(v, et_main_safe_key)//当前焦点在自定义键盘EditText
+                    && !KeyboardTool.isTouchView(new View[]{et_main_safe_key}, ev)) {//且没有触摸在自定义键盘EditText
+                if (keyboardUtil != null)//隐藏自定义键盘
+                    keyboardUtil.hide();
+                v.clearFocus();//清空焦点
+            }
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    /**
+     * 检查当前apk的sha值
+     * 建议这里从服务器中获取哈希值然后进行对比校验
+     * @return 当前apk的sha值,异常时为空字符串
+     */
+    public String checkApkSha() {
+        String apkPath = getPackageCodePath();
+        MessageDigest msgDigest;
+        try {
+            msgDigest = MessageDigest.getInstance("SHA-1");
+            byte[] bytes = new byte[1024];
+            int byteCount;
+            FileInputStream fis = new FileInputStream(new File(apkPath));
+            while ((byteCount = fis.read(bytes)) > 0) {
+                msgDigest.update(bytes, 0, byteCount);
+            }
+            BigInteger bi = new BigInteger(1, msgDigest.digest());
+            String sha = bi.toString(16);
+            fis.close();
+            Log.i(MainActivity.this.getClass().getName(),"==sha-->"+sha);
+            return sha;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
 
     /**
      * 根据bugly的实现判断root环境
+     *
      * @return true:是root环境，false:不是root环境
      */
     public boolean checkRoot() {
